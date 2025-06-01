338f47f19bd5a89b58311ed379e4d8c35921957b
Wasi Mirza <wasidevil@gmail.com>
2025-06-02 00:18:19 +0530

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { HashRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <App />
+      <Analytics />
     </HashRouter>
   </StrictMode>
 );
