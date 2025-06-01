1db59c03b81b8fd55a0bb4f0073472c551a96a3e
Wasi Mirza <wasidevil@gmail.com>
2025-06-02 00:12:31 +0530

    Fix base path for Vercel root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/', // or just '' (empty string) also works
   assetsInclude: ['**/*.riv'],
   plugins: [react()],
 })
